affa88337af53cf93d1c157bd04d436706b1fcc9
HoldenFolk <holdenfolk1@gmail.com>
2022-11-23 10:50:59 -0500

    Add further component files and add global css vars

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Header from './components/header/Header.jsx'
+import Nav from './components/nav/Nav.jsx'
+import About from './components/about/About.jsx'
+import Experience from './components/experience/Experience.jsx'
+import ThisSite from './components/thisSite/ThisSite.jsx'
+import Contact from './components/contact/Contact.jsx'
+import Footer from './components/footer/Footer.jsx'
+
+
 
 const App = () => {
   return (
-    <div>This is my app</div>
+    <>
+      <Header/>
+      <Nav/>
+      <About/>
+      <Experience/>
+      <ThisSite/>
+      <Contact/>
+      <Footer/>
+    </>
   )
 }
 
