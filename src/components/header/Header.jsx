e3d0d1c1be6014595f9610b3570fcdb211cc050b
HoldenFolk <holdenfolk1@gmail.com>
2022-11-24 15:55:16 -0500

    Fix profile image and add some basic styling

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,22 +2,25 @@ import React from 'react'
 import './header.css'
 import CTA from "./CTA"
 import HeaderSocials from "./HeaderSocials"
-import Headshot from "../../assets/Headshot.JPG"
+import ProfilePic from "../../assets/ProfilePic.png"
 
 const Header = () => {
   return (
     <header>
         <div className="container header__container">
+
             <h5>Hello I'm</h5>
             <h1>Holden Folk</h1>
             <h5 className="text-light">Lorem Ipsum</h5>
+            
             <CTA/>
 
-              <div className="headshot">
-                <img src={Headshot} alt="me"/>
-              </div>
+            <div className="headshot">
+              <img src={ProfilePic} alt="headshot"/>
+            </div>
 
             <HeaderSocials/>
+            
         </div>
     </header>
   )
