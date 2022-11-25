a578cd565601c1c9e1613cc3e2281e6291064308
HoldenFolk <holdenfolk1@gmail.com>
2022-11-25 15:41:27 -0500

    Fix image styling and start creating about section of page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './about.css'
+import Headshot from '../../assets/Headshot.png'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about' className='container about__container'>
+      <h2> About me: </h2>
+
+      <div className="workImage">
+              <img src={Headshot} alt="headshot"/>
+      </div>
+
+      <h5> Lorem Ipsum </h5>
+
+    </section>
   )
 }
 
