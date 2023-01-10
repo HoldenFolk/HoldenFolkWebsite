cec3475c6077900df33ebac17b9d4fd8e1ad1866
HoldenFolk <holdenfolk1@gmail.com>
2023-01-09 22:52:21 -0500

    Add first draft styling to about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,18 +1,57 @@
 import React from 'react'
 import './about.css'
 import Headshot from '../../assets/altas_people.jpg'
+import {GiClassicalKnowledge} from 'react-icons/gi'
+import {IoMdSchool} from 'react-icons/io'
+import {RiLightbulbLine} from 'react-icons/ri'
 
 const About = () => {
   return (
-    <section id='about' className='container about__container'>
+    <section id='about' >
+
       <h2> About me: </h2>
-       
+
+     <div className='container about__container'>
+     <div className = "about__me">
         <div className="workImage">
-               <img src={Headshot} alt="headshot"/>
+               <img src={Headshot} alt="work image"/>
+        </div>
+      </div>
+      
+
+      <div className = "about__content">
+        <div className = "about__tags">
+
+          <article className='about__tag'> 
+            <GiClassicalKnowledge className="about__icon"/> 
+            <h5> Experience </h5>
+            <small> Has done x </small>
+          </article> 
+
+          <article className= "about__tag"> 
+            <IoMdSchool className='about__icon'/>
+            <h5> Education </h5>
+            <small> Lorem Ipsum </small>
+          </article>
+
+          <article className='about__tag'>
+             <RiLightbulbLine className='about__icon'/>
+             <h5> Intrests </h5>
+             <small> Lorem Ipsum </small>
+          </article> 
         </div>
 
-        <h5> Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem IpsumLorem IpsumLorem Ipsum </h5>
+        <p> 
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+          Eos assumenda dicta nemo vitae ullam quae necessitatibus?
+           Eveniet voluptatum incidunt consequuntur provident? Ipsum placeat,
+            quaerat reprehenderit amet soluta sunt ducimus mollitia?
+        </p>
 
+        <a href="#contact"  className='btn btn-primary'> </a>
+
+        </div>
+      </div>
     </section>
   )
 }
