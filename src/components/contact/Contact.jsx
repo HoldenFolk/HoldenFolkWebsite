eb30f2ffb4142919e8574f947b8622ea264178f4
HoldenFolk <holdenfolk1@gmail.com>
2023-02-22 14:33:47 -0500

    Add styling to the contact section of website

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './contact.css'
 import { AiOutlineMail } from 'react-icons/ai'
+import {AiOutlineWhatsApp} from 'react-icons/ai'
+import {AiOutlineLinkedin} from 'react-icons/ai'
+
 
 const Contact = () => {
   return (
@@ -8,22 +11,22 @@ const Contact = () => {
       <h5>Get In Touch</h5>
       <h2> Contact Me</h2>
 
-    <div className='conainter contact__container'> 
+    <div className='container contact__container'> 
       <div className="contact__options">
         <article className='contact__option'> 
-        <AiOutlineMail/> 
+        <AiOutlineMail className='contact__option-icon'/> 
           <h4>Email</h4>
           <h5>holdenfolk1@gmail.com</h5>
           <a href="mailto:holdenfolk1@gmail.com" target='_blank'>Send a message</a>
         </article>
         <article className='contact__option'> 
-        <AiOutlineMail/> 
+        <AiOutlineWhatsApp className='contact__option-icon'/> 
           <h4>LinhkedIn</h4>
           <h5>Holden Folk</h5>
           <a href="https://www.linkedin.com/in/holden-folk-995b8220a" target='_blank'>Send a message</a>
         </article>
         <article className='contact__option'> 
-        <AiOutlineMail/> 
+        <AiOutlineLinkedin className='contact__option-icon'/> 
           <h4>Phone/WhatsApp</h4>
           <h5>+1 647-967-2658</h5>
           <a href="https://api.whatsapp.com/send?phone=16479672658" target='_blank'>Send a message</a>
