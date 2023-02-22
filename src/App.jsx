8000ee892a946ab2f35e0194eacd14b99ef91b33
HoldenFolk <holdenfolk1@gmail.com>
2023-02-22 17:22:21 -0500

    fix minor import bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import Header from './components/header/Header.jsx'
 import Nav from './components/nav/Nav.jsx'
 import About from './components/about/About.jsx'
-import Experience from './components/experience/Experience.jsx'
-import ThisSite from './components/thisSite/ThisSite.jsx'
 import Contact from './components/contact/Contact.jsx'
 import Footer from './components/footer/Footer.jsx'
 
